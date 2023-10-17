f29ef04d33dd1ca48757d7e6bc2139e45865a0d1
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2023-10-17 12:51:53 +0300

    Add script to filter and print parsed data as html

---
+++ read_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='Dividend Search - To HTML',
+                    description='Search for companies paying a dividend')
+parser.add_argument("filename", help="CSV file containing the parsed metrics")
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.filename)
+
+df = df[df["comment"] == "ok"]
+df = df[df["growth_tot"] > 0.79]
+df = df[df["Debt Ratio"] < 5]
+df = df[df["ROE"] > 0]
+df = df[df["Sector"].str.contains("Financial Services") == False]
+
+cols = list(df.columns[0:list(df.columns).index("outliers")]) + list(df.columns[list(df.columns).index("outliers") + 1:]) + ["outliers"]
+df = df[cols]
+df = df.sort_values(by="symbol")
+
+print(df.to_html(index=False))
