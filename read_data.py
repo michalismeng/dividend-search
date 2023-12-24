88478a468df6ba5c1c10ebc20b95e58ad942b875
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2023-12-24 14:02:38 +0200

    Allow reading scratch data without filtering

--- read_data.py
+++ read_data.py
@@ -5,16 +5,18 @@ parser = argparse.ArgumentParser(
                     prog='read_data',
                     description='Produce HTML from CSV file with financial metrics')
 parser.add_argument("filename", help="Input CSV file containing the parsed metrics")
+parser.add_argument("--filter", action=argparse.BooleanOptionalAction, default=True, help="Apply any filters to dataset")
 
 args = parser.parse_args()
 
 df = pd.read_csv(args.filename)
 
-df = df[df["comment"] == "ok"]
-df = df[df["growth_yy"] > 0.07]
-df = df[df["Debt Ratio"] < 5]
-df = df[df["ROE"] > 0]
-df = df[df["Sector"].str.contains("Financial Services") == False]
+if args.filter:
+    df = df[df["comment"] == "ok"]
+    df = df[df["growth_yy"] > 0.07]
+    df = df[df["Debt Ratio"] < 5]
+    df = df[df["ROE"] > 0]
+    df = df[df["Sector"].str.contains("Financial Services") == False]
 
 cols = list(df.columns[0:list(df.columns).index("outliers")]) + list(df.columns[list(df.columns).index("outliers") + 1:]) + ["outliers"]
 df = df[cols]
